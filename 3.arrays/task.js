d30615a430de46844616a40cf93271f91b623303
MikhailD <Doroshenko_904@mail.ru>
2023-12-22 22:08:40 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    if (arr1.length === arr2.length) {
+        return arr1.every((el, i) => el === arr2[i])
+      } else if(arr1.length !== arr2.length) {
+              return false
+        }
+      }
+     
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (!users || !Array.isArray(users) || users.length === 0 || !gender || typeof gender !== 'string') {
+        return 0;
+        }
+        
+        const filteredUsers = users.filter(user => user.gender === gender);
+        if (filteredUsers.length === 0) {
+        return 0;
+        }
+        
+        const totalAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+        const averageAge = totalAge / filteredUsers.length;
+        
+        return averageAge;
 }
